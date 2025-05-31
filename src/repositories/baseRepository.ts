20f34d8cb972ab8fee7f3e53dbc0d7268c3c9f00
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-06-01 07:43:03 +0800

    add try catch

--- src/repositories/baseRepository.ts
+++ src/repositories/baseRepository.ts
@@ -2,6 +2,7 @@ import Knex from 'knex';
 import { dbInstance } from '../services/PostgresService';
 import { IBaseRepository } from '../interfaces/IBaseRepository';
 import { TableName } from '../interfaces/TableName';
+import { DBError } from '../utils/DBError';
 
 type BaseRepositoryParameters = {
   tableName: TableName;
@@ -19,56 +20,90 @@ class BaseRepository<T> implements IBaseRepository<T> {
   }
 
   async create(data: T): Promise<T> {
-    const result = await this.knex(this.tableName).insert(data).returning('*');
-    return result[0] as T;
+    try {
+      const result = await this.knex(this.tableName)
+        .insert(data)
+        .returning('*');
+      return result[0] as T;
+    } catch (ex: any) {
+      throw new DBError(ex);
+    }
   }
 
   async update(id: string, attrs: Partial<T>): Promise<T> {
-    const result = await this.knex(this.tableName)
-      .update(attrs)
-      .where({ [this.primaryKey]: id })
-      .returning('*');
-    return result[0] as T;
+    try {
+      const result = await this.knex(this.tableName)
+        .update(attrs)
+        .where({ [this.primaryKey]: id })
+        .returning('*');
+      return result[0] as T;
+    } catch (ex: any) {
+      throw new DBError(ex);
+    }
   }
 
   async find(limit: number, offset: number): Promise<T[]> {
-    const result = await this.knex(this.tableName)
-      .select('*')
-      .orderBy(this.primaryKey)
-      .limit(limit)
-      .offset(offset);
-    return result as T[];
+    try {
+      const result = await this.knex(this.tableName)
+        .select('*')
+        .orderBy(this.primaryKey)
+        .limit(limit)
+        .offset(offset);
+      return result as T[];
+    } catch (ex: any) {
+      throw new DBError(ex);
+    }
   }
 
   async findBy(attrs: Partial<T>): Promise<T[]> {
-    const result = await this.knex(this.tableName).select('*').where(attrs);
-    return result as T[];
+    try {
+      const result = await this.knex(this.tableName).select('*').where(attrs);
+      return result as T[];
+    } catch (ex: any) {
+      throw new DBError(ex);
+    }
   }
 
   async findById(id: string): Promise<T> {
-    const result = await this.knex(this.tableName)
-      .select('*')
-      .where({ [this.primaryKey]: id })
-      .returning('*');
-    return result[0] as T;
+    try {
+      const result = await this.knex(this.tableName)
+        .select('*')
+        .where({ [this.primaryKey]: id })
+        .returning('*');
+      return result[0] as T;
+    } catch (ex: any) {
+      throw new DBError(ex);
+    }
   }
 
   async delete(id: string): Promise<T[]> {
-    const result = await this.knex(this.tableName)
-      .delete()
-      .where({ [this.primaryKey]: id })
-      .returning('*');
-    return result as T[];
+    try {
+      const result = await this.knex(this.tableName)
+        .delete()
+        .where({ [this.primaryKey]: id })
+        .returning('*');
+      return result as T[];
+    } catch (ex: any) {
+      throw new DBError(ex);
+    }
   }
 
   async count(): Promise<number> {
-    const result = await this.knex(this.tableName).count('*');
-    return Number(result[0].count);
+    try {
+      const result = await this.knex(this.tableName).count('*');
+      return Number(result[0].count);
+    } catch (ex: any) {
+      throw new DBError(ex);
+    }
   }
 
   async countBy(attrs: Partial<T>): Promise<number> {
-    const result = await this.knex(this.tableName).count('*').where(attrs);
-    return Number(result[0].count);
+    try {
+      const result = await this.knex(this.tableName).count('*').where(attrs);
+      return Number(result[0].count);
+    } catch (ex: any) {
+      throw new DBError(ex);
+    }
   }
 }
 
