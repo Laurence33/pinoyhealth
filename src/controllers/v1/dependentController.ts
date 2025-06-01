02575cbdf5fa02cceaaf519bf613a36442d86d1d
Laurence Cortez <laurencecortez26.lc@gmail.com>
2025-06-01 14:11:39 +0800

    cleanup

--- src/controllers/v1/dependentController.ts
+++ src/controllers/v1/dependentController.ts
@@ -6,21 +6,6 @@ import { sendHttpResponse } from '../../utils/httpResponse';
 class DependentController {
   constructor(private interactor: IDependentInteractor) {}
 
-  // onCreateDependent = async (
-  //   req: Request,
-  //   res: Response,
-  //   _nxt: NextFunction,
-  // ): Promise<any> => {
-  //   const { body } = req;
-  //   const dependent = await this.interactor.createDependent(body);
-
-  //   return sendHttpResponse({
-  //     res,
-  //     statusCode: HttpCode.CREATED,
-  //     data: dependent,
-  //   });
-  // };
-
   onGetDependent = async (
     req: Request,
     res: Response,
@@ -54,57 +39,6 @@ class DependentController {
       data: result,
     });
   };
-
-  // onReplaceDependent = async (
-  //   req: Request,
-  //   res: Response,
-  //   _nxt: NextFunction,
-  // ): Promise<any> => {
-  //   const { id } = req.params;
-  //   const fields = req.body;
-  //   const updatedDependent = await this.interactor.updateDependent(id, fields);
-
-  //   return sendHttpResponse({
-  //     res,
-  //     statusCode: HttpCode.SUCCESS,
-  //     data: updatedDependent,
-  //   });
-  // };
-  // onUpdateDependent = async (
-  //   req: Request,
-  //   res: Response,
-  //   _nxt: NextFunction,
-  // ): Promise<any> => {
-  //   const { id } = req.params;
-  //   const fields = req.body;
-  //   const updatedDependent = await this.interactor.updateDependent(id, fields);
-
-  //   return sendHttpResponse({
-  //     res,
-  //     statusCode: HttpCode.SUCCESS,
-  //     data: updatedDependent,
-  //   });
-  // };
-  // onDeleteDependent = async (
-  //   req: Request,
-  //   res: Response,
-  //   _nxt: NextFunction,
-  // ): Promise<any> => {
-  //   const { id } = req.params;
-  //   if (!id) {
-  //     throw new Error('Id is required');
-  //   }
-
-  //   // TODO: check for contribution
-  //   // TODO: check for dependents
-
-  //   const deletedDependent = await this.interactor.deleteDependent(id);
-  //   return sendHttpResponse({
-  //     res,
-  //     statusCode: HttpCode.SUCCESS,
-  //     data: deletedDependent,
-  //   });
-  // };
 }
 
 export { DependentController };
